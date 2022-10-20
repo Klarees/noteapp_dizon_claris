cd90fd89b7d30168ffc8386b8c24788b59243010
Claris <dizonclariskimmy@gmail.com>
2022-10-20 18:55:08 +0800

    added add.js module

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
+const add = require('./add')
 const read = require('./read')
 const write = require('./write')
 
-const data = process.argv
+const cmd = process.argv
 
 var note = {}
 
-if(data[2] == 'add') {
+if(cmd[2] == 'add') {
    
     note = {
-        id: data[3],
-        title: data[4],
-        body: data[5]
+        id: cmd[3],
+        title: cmd[4],
+        body: cmd[5]
     }
    
     var oldNote = read()
    
     add(note, oldNote);
 }
-if(data[2] == 'read') {
+if(cmd[2] == 'read') {
     
     const present = require('./present')
 
